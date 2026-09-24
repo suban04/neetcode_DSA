ed1fa38de68d1210387614ab14bfc2e61ba89264
Suban Shaikh <subanshaikh134@gmail.com>
2026-09-24 17:08:10 +0530

    Add: valid-perfect-square - submission-1

---
+++ Data Structures & Algorithms/valid-perfect-square/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+            int left =1 ;
+            int right= num;
+            while(left<=right){
+                int  mid =left+ (right - left) /2;
+                if(mid ==num /mid && num %  mid == 0){
+                    return true;
+                }
+                if(mid<num/mid){
+                    left =mid +1;
+                }else{
+                    right =mid-1;
+                }
+            }
+           
+        return false;
+    }
+}
